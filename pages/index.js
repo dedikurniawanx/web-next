22341812b02c8fed4241da4c7b364b8bf82a20e5
dedikurniawanx <dedikurniawanx@gmail.com>
2022-12-08 07:58:39 +0700

    summary

--- pages/index.js
+++ pages/index.js
@@ -190,30 +190,13 @@ export default function Home() {
         <section id="resume" className="resume">
           <div className="container" data-aos="fade-up">
             <div className="section-title">
-              <h2>Resume</h2>
+              <h2>Sumary</h2>
               <div className="row">
                 <div className="col-lg-6">
-                  <h3 className="resume-title">Sumary</h3>
-                  <div className="resume-item pb-0">
-                    <h4>Brandon Johnson</h4>
-                    <p>
-                      <em>
-                        Innovative and deadline-driven Graphic Designer with 3+
-                        years of experience designing and developing
-                        user-centered digital/print marketing material from
-                        initial concept to final, polished deliverable.
-                      </em>
-                    </p>
-                    <ul>
-                      <li>Portland par 127,Orlando, FL</li>
-                      <li>(123) 456-7891</li>
-                      <li>alice.barkley@example.com</li>
-                    </ul>
-                  </div>
                   <h3 className="resume-title">Education</h3>
                   <div className="resume-item">
-                    <h4>Master of Fine Arts &amp; Graphic Design</h4>
-                    <h5>2015 - 2016</h5>
+                    <h4>SD Negeri 2 Tegalrejo</h4>
+                    <h5>2010 - 2014</h5>
                     <p>
                       <em>Rochester Institute of Technology, Rochester, NY</em>
                     </p>
@@ -224,7 +207,33 @@ export default function Home() {
                     </p>
                   </div>
                   <div className="resume-item">
-                    <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
+                    <h4>MTs DIPONEGORO TEGALSARI </h4>
+                    <h5>2010 - 2014</h5>
+                    <p>
+                      <em>Rochester Institute of Technology, Rochester, NY</em>
+                    </p>
+                    <p>
+                      Quia nobis sequi est occaecati aut. Repudiandae et iusto
+                      quae reiciendis et quis Eius vel ratione eius unde vitae
+                      rerum voluptates asperiores voluptatem Earum molestiae
+                      consequatur neque etlon sader mart dila
+                    </p>
+                  </div>
+                  <div className="resume-item">
+                    <h4>Smk Negeri 2 Tegalsari</h4>
+                    <h5>2010 - 2014</h5>
+                    <p>
+                      <em>Rochester Institute of Technology, Rochester, NY</em>
+                    </p>
+                    <p>
+                      Quia nobis sequi est occaecati aut. Repudiandae et iusto
+                      quae reiciendis et quis Eius vel ratione eius unde vitae
+                      rerum voluptates asperiores voluptatem Earum molestiae
+                      consequatur neque etlon sader mart dila
+                    </p>
+                  </div>
+                  <div className="resume-item">
+                    <h4>Sekolah Tinggi Ilmu Komputer Pgri Banyuwangi</h4>
                     <h5>2010 - 2014</h5>
                     <p>
                       <em>Rochester Institute of Technology, Rochester, NY</em>
@@ -238,7 +247,7 @@ export default function Home() {
                   </div>
                 </div>
                 <div className="col-lg-6">
-                  <h3 className="resume-title">Professional Experience</h3>
+                  <h3 className="resume-title">Work Experience</h3>
                   <div className="resume-item">
                     <h4>Senior graphic design specialist</h4>
                     <h5>2019 - Present</h5>
@@ -250,18 +259,6 @@ export default function Home() {
                         Lead in the design, development, and implementation of
                         the graphic, layout, and production communication
                         materials
-                      </li>
-                      <li>
-                        Delegate tasks to the 7 members of the design team and
-                        provide counsel on all aspects of the project.
-                      </li>
-                      <li>
-                        Supervise the assessment of all graphic materials in
-                        order to ensure quality and accuracy of the design
-                      </li>
-                      <li>
-                        Oversee the efficient use of production project budgets
-                        ranging from $2,000 - $25,000
                       </li>
                     </ul>
                   </div>
@@ -276,18 +273,6 @@ export default function Home() {
                         Developed numerous marketing programs (logos,
                         brochures,infographics, presentations, and
                         advertisements).
-                      </li>
-                      <li>
-                        Managed up to 5 projects or tasks at a given time while
-                        under pressure
-                      </li>
-                      <li>
-                        Recommended and consulted with clients on the most
-                        appropriate graphic design
-                      </li>
-                      <li>
-                        Created 4+ design presentations and proposals a month
-                        for clients and account managers
                       </li>
                     </ul>
                   </div>
