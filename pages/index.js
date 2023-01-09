78528e7fe266aff54caedd15d3b85116ef32cf8b
dedikurniawanx <dedikurniawanx@gmail.com>
2023-01-09 19:26:48 +0700

    fix profile

--- pages/index.js
+++ pages/index.js
@@ -14,13 +14,13 @@ export default function Home({ blog, portofolio }) {
                 </a>
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link href="/#about">
                 <a className="nav-link scrollto">
                   <i className="bx bx-user" /> <span>About</span>
                 </a>
               </Link>
-            </li>
+            </li> */}
             <li>
               <Link href="/#resume">
                 <a className="nav-link scrollto">
@@ -37,174 +37,31 @@ export default function Home({ blog, portofolio }) {
         </nav>
       </header>
       <section id="hero" className="d-flex flex-column justify-content-center">
-        <div className="container" data-aos="zoom-in" data-aos-delay={100}>
-          <h1>Dedi Kurniawan</h1>
-          <p>
-            I'm
-            <span
-              className="typed"
-              data-typed-items="Designer, Developer, Photographer"
-            />
-          </p>
-          <div className="social-links">
-            <a
-              href="https://www.instagram.com/xdkurniawn/"
-              target="_blank"
-              className="instagram"
-            >
-              <i className="bx bxl-instagram" />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/dedi-kurniawan-724094259/"
-              target="_blank"
-              className="linkedin"
-            >
-              <i className="bx bxl-linkedin" />
-            </a>
-            <a
-              href="https://github.com/dedikurniawanx"
-              target="_blank"
-              className="github"
-            >
-              <i className="bx bxl-github" />
-            </a>
-
-            <a
-              href="https://twitter.com/xdkurniawn"
-              target="_blank"
-              className="twitter"
-            >
-              <i className="bx bxl-twitter" />
-            </a>
+        <div className="container row" data-aos="zoom-in" data-aos-delay={100}>
+          <div className="row">
+            <div className="col-md-8 align-self-center">
+              <h1>Dedi Kurniawan</h1>
+              <h2>
+                <span
+                  className="typed"
+                  data-typed-items="Front-End Developer,Designer"
+                />
+              </h2>
+              <button type="button" class="btn btn-primary rounded-pill">
+                Contact Me
+              </button>
+            </div>
+            <div className="col-md-4">
+              <img
+                src="assets/img/saya.webp"
+                className="img-fluid profile"
+                alt
+              />
+            </div>
           </div>
         </div>
       </section>
       <main id="main">
-        <section id="about" className="about">
-          <div className="container" data-aos="fade-up">
-            <div className="section-title">
-              <h2>About</h2>
-            </div>
-            <div className="row">
-              <div className="col-lg-4">
-                <img src="assets/img/saya.webp" className="img-fluid" alt />
-              </div>
-              <div className="col-lg-8 pt-4 pt-lg-0 content">
-                <h3>Designer &amp; Web Developer</h3>
-                <p className="">
-                  Halo perkenalkan, nama saya Dedi Kurniawan, saya adalah
-                  mahasiswa jurusan Teknik Informatka tahun 2020. Teknik
-                  Informatika merupakan bidang yang sangat menyenangkan terutama
-                  bagi mereka yang memiliki minat dan hobi di bidang teknologi.
-                </p>
-                <div className="row">
-                  <div className="col-lg-6">
-                    <ul>
-                      <li>
-                        <i className="" />
-                        <strong>City:</strong>
-                        <span>Banyuwangi, Jawatimur, Indonesia</span>
-                      </li>
-                      <li>
-                        <i className="" /> <strong>Hoby:</strong>
-                        <span>Maen Game, Fotografi</span>
-                      </li>
-                      <li>
-                        <i className="" />
-                        <strong>Birthday:</strong> <span>June 2001</span>
-                      </li>
-                      <li>
-                        <i className="" /> <strong>Age:</strong>
-                        <span>21</span>
-                      </li>
-
-                      <li>
-                        <i className="" />
-                        <strong>Freelance:</strong> <span>-</span>
-                      </li>
-                    </ul>
-                  </div>
-                  {/* <div className="col-lg-6">
-                    <ul>
-                      <li>
-                        <i className="bi bi-chevron-right" />
-                        <strong>Degree:</strong> <span>Junior</span>
-                      </li>
-
-                      <li>
-                        <i className="bi bi-chevron-right" />
-                        <strong>Website:</strong>{" "}
-                        <span>www.dedikurniawan.xyz</span>
-                      </li>
-                      <li>
-                        <i className="bi bi-chevron-right" />
-                        <strong>Phone:</strong> <span>+6234567890</span>
-                      </li>
-                      <li>
-                        <i className="bi bi-chevron-right" />
-                        <strong>Email:</strong>
-                        <span>dedikurniawanx@gmail.com</span>
-                      </li>
-                    </ul>
-                  </div> */}
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section id="skills" className="skills section-bg">
-          <div className="container" data-aos="fade-up">
-            <div className="section-title">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                vulputate sem felis, a ullamcorper odio blandit sit amet.
-                Vestibulum fermentum risus lorem, ullamcorper luctus metus
-                sollicitudin luctus. Cras sed risus.
-              </p>
-            </div>
-            <div className="row skills-content justify-content-center">
-              <div className="col-md-9 row">
-                <div className="col-md-3 d-flex justify-content-center align-items-center  flex-column ">
-                  <img
-                    className="m-img"
-                    src="/assets/img/html_icon.webp"
-                    height={80}
-                    width={80}
-                  ></img>
-                  <p className="text-center">HTML</p>
-                </div>
-                <div className="col-md-3 d-flex justify-content-center align-items-center flex-column ">
-                  <img
-                    className="m-img"
-                    src="/assets/img/css_icon.webp"
-                    height={80}
-                    width={80}
-                  ></img>
-                  <p className="text-center">Css</p>
-                </div>
-
-                <div className="col-md-3 d-flex justify-content-center align-items-center  flex-column ">
-                  <img
-                    className="m-img"
-                    src="/assets/img/boostrap.webp"
-                    height={80}
-                    width={80}
-                  ></img>
-                  <p className="text-center">Bootstrap</p>
-                </div>
-                <div className="col-md-3 d-flex justify-content-center align-items-center  flex-column ">
-                  <img
-                    className="m-img"
-                    src="/assets/img/javascript_icon.webp"
-                    height={80}
-                    width={80}
-                  ></img>
-                  <p className="text-center">Javascript</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
         {/* <section id="resume" className="resume">
           <div className="container" data-aos="fade-up">
             <div className="section-title">
@@ -288,8 +145,7 @@ export default function Home({ blog, portofolio }) {
         <section id="resume" className="portfolio section-bg">
           <div className="container" data-aos="fade-up">
             <div className="section-title">
-              <h2>Portofolio</h2>
-              <p>Ini Adalah Tugas Desain Yang Pernah Saya Buat</p>
+              <h2>Come see my portfolio</h2>
             </div>
             <div className="row justify-content-center">
               {portofolio.result.map((item) => {
@@ -302,53 +158,11 @@ export default function Home({ blog, portofolio }) {
                         className="card-img-top rounded-0"
                         alt="..."
                       />
-                      <div className="card-body">
+                      {/* <div className="card-body">
                         <h5 class="card-title text-center font-weight-bold pt-3">
                           {item.name}
                         </h5>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-            <div className="row mt-5">
-              <div className="text-center">
-                <Link href="/blog">
-                  <a href="#">
-                    {/* <button type="button" class="btn btn-primary rounded-pill">
-                      Show
-                    </button> */}
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="container" data-aos="fade-up">
-            <div className="section-title">
-              <h2>Blog</h2>
-              <p>Artikel Terbaru Saya</p>
-            </div>
-            <div className="row justify-content-center">
-              {blog.result.map((item) => {
-                const image = item.poster.asset._ref.split("-");
-                console.log(`${image[1]}-${image[2]}.${image[3]}`);
-
-                return (
-                  <div className="col-lg-5 col-md-6">
-                    <div className="card shadow-sm p-3 mb-5 m-2 bg-body rounded-0">
-                      <img
-                        src={`https://cdn.sanity.io/images/sbpqwv31/production/${image[1]}-${image[2]}.${image[3]}`}
-                        className="card-img-top rounded-0"
-                        alt="..."
-                      />
-                      <div className="card-body">
-                        <h5 class="card-title font-weight-bold">
-                          {" "}
-                          <strong>{item.name}</strong>
-                        </h5>
-                        <p className="card-text">{item.description}</p>
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 );
@@ -369,7 +183,7 @@ export default function Home({ blog, portofolio }) {
         </section>
       </main>
       <footer id="footer">
-        <div className="container mt-5">
+        {/* <div className="container mt-5">
           <div className="copyright text-center">
             © Copyright{" "}
             <strong>
@@ -377,10 +191,10 @@ export default function Home({ blog, portofolio }) {
             </strong>
             . All Rights Reserved
           </div>
-          {/* <div className="credits text-center">
+          <div className="credits text-center">
             Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-          </div> */}
-        </div>
+          </div>
+        </div> */}
       </footer>
     </div>
   );
@@ -394,7 +208,6 @@ export async function getServerSideProps(context) {
   );
   const portofolio = await responses.data;
   const blog = await response.data;
-  console.log(portofolio);
   return {
     props: { blog, portofolio }, // will be passed to the page component as props
   };
