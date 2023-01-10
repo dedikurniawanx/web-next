7188a5a9b20435a6cf2dbb5ff3479f4da0465b2f
dedikurniawanx <dedikurniawanx@gmail.com>
2023-01-10 08:25:39 +0700

    fix favicon

--- pages/index.js
+++ pages/index.js
@@ -21,23 +21,23 @@ export default function Home({ blog, portofolio }) {
                 </a>
               </Link>
             </li> */}
-            <li>
+            {/* <li>
               <Link href="/#resume">
                 <a className="nav-link scrollto">
                   <i className="bx bx-file-blank" /> <span>Resume</span>
                 </a>
               </Link>
-            </li>
-            {/* <li>
-              <a href="/#blog" className="nav-link scrollto">
-                <i className="bx bx-book-content" /> <span>Blog</span>
-              </a>
             </li> */}
+            <li>
+              <a href="/#portofolio" className="nav-link scrollto">
+                <i className="bx bx-book-content" /> <span>Portofolio</span>
+              </a>
+            </li>
           </ul>
         </nav>
       </header>
       <section id="hero" className="d-flex flex-column justify-content-center">
-        <div className="container row" data-aos="zoom-in" data-aos-delay={100}>
+        <div className="container" data-aos="zoom-in" data-aos-delay={100}>
           <div className="row">
             <div className="col-md-8 align-self-center">
               <h1>Dedi Kurniawan</h1>
@@ -62,87 +62,7 @@ export default function Home({ blog, portofolio }) {
         </div>
       </section>
       <main id="main">
-        {/* <section id="resume" className="resume">
-          <div className="container" data-aos="fade-up">
-            <div className="section-title">
-              <h2>Sumary</h2>
-              <div className="row">
-                <div className="col-lg-6">
-                  <h3 className="resume-title">Education</h3>
-                  <div className="resume-item">
-                    <h4>SD Negeri 2 Tegalrejo</h4>
-                    <h5>2010 - 2014</h5>
-                    <p>
-                      <em>Rochester Institute of Technology, Rochester, NY</em>
-                    </p>
-                    <p>
-                      Qui deserunt veniam. Et sed aliquam labore tempore sed
-                      quisquam iusto autem sit. Ea vero voluptatum qui ut
-                      dignissimos deleniti nerada porti sand markend
-                    </p>
-                  </div>
-                  <div className="resume-item">
-                    <h4>MTs DIPONEGORO TEGALSARI </h4>
-                    <h5>2010 - 2014</h5>
-                    <p>
-                      <em>Rochester Institute of Technology, Rochester, NY</em>
-                    </p>
-                    <p>
-                      Quia nobis sequi est occaecati aut. Repudiandae et iusto
-                      quae reiciendis et quis Eius vel ratione eius unde vitae
-                      rerum voluptates asperiores voluptatem Earum molestiae
-                      consequatur neque etlon sader mart dila
-                    </p>
-                  </div>
-                  <div className="resume-item">
-                    <h4>Smk Negeri 2 Tegalsari</h4>
-                    <h5>2010 - 2014</h5>
-                    <p>
-                      <em>Rochester Institute of Technology, Rochester, NY</em>
-                    </p>
-                    <p>
-                      Quia nobis sequi est occaecati aut. Repudiandae et iusto
-                      quae reiciendis et quis Eius vel ratione eius unde vitae
-                      rerum voluptates asperiores voluptatem Earum molestiae
-                      consequatur neque etlon sader mart dila
-                    </p>
-                  </div>
-                  <div className="resume-item">
-                    <h4>Sekolah Tinggi Ilmu Komputer Pgri Banyuwangi</h4>
-                    <h5>2010 - 2014</h5>
-                    <p>
-                      <em>Rochester Institute of Technology, Rochester, NY</em>
-                    </p>
-                    <p>
-                      Quia nobis sequi est occaecati aut. Repudiandae et iusto
-                      quae reiciendis et quis Eius vel ratione eius unde vitae
-                      rerum voluptates asperiores voluptatem Earum molestiae
-                      consequatur neque etlon sader mart dila
-                    </p>
-                  </div>
-                </div>
-                <div className="col-lg-6">
-                  <h3 className="resume-title">Work Experience</h3>
-                  <div className="resume-item">
-                    <h4>Praktek Kerja Lapangan di percetakan Mca.com</h4>
-                    <h5>2010 - 2014</h5>
-                    <p>
-                      <em>Rochester Institute of Technology, Rochester, NY</em>
-                    </p>
-                    <ul>
-                      <li>
-                        Lead in the design, development, and implementation of
-                        the graphic, layout, and production communication
-                        materials
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section> */}
-        <section id="resume" className="portfolio section-bg">
+        <section id="portofolio" className="portfolio section-bg">
           <div className="container" data-aos="fade-up">
             <div className="section-title">
               <h2>Come see my portfolio</h2>
@@ -151,8 +71,8 @@ export default function Home({ blog, portofolio }) {
               {portofolio.result.map((item) => {
                 const image = item.image.asset._ref.split("-");
                 return (
-                  <div className="col-lg-6 col-md-6">
-                    <div className="card shadow-sm p-3 mb-5 m-2 bg-body rounded-0">
+                  <div className="col-lg-4 col-md-6">
+                    <div className="card shadow-sm p-2 bg-body rounded-0 shadow-sm">
                       <img
                         src={`https://cdn.sanity.io/images/sbpqwv31/production/${image[1]}-${image[2]}.${image[3]}`}
                         className="card-img-top rounded-0"
@@ -177,6 +97,32 @@ export default function Home({ blog, portofolio }) {
                     </button> */}
                   </a>
                 </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="resume" className="resume">
+          <div className="container" data-aos="fade-up">
+            <div className="section-title">
+              <h2>Sumary</h2>
+              <div className="row">
+                <div className="col-lg">
+                  <h3 className="resume-title">Work Experience</h3>
+                  <div className="resume-item">
+                    <h4>-</h4>
+                    <h5>2010 - 2014</h5>
+                    <p>
+                      <em>Rochester Institute of Technology, Rochester, NY</em>
+                    </p>
+                    <ul>
+                      <li>
+                        Lead in the design, development, and implementation of
+                        the graphic, layout, and production communication
+                        materials
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
